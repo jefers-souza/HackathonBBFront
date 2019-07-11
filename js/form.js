8fa1a402220edc2b4bb419f5d34cb815dda93056
Gabriela Guedes <zooiv3rde@gmail.com>
2019-07-10 23:05:32 -0300

    v2

--- js/form.js
+++ js/form.js
@@ -1,17 +1,28 @@
-//função para adicionar um novo paciente.
+//função para adicionar um novo pessoa.
 botaoAdicionar = document.querySelector("#adicionar-reserva");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function(event) {//adicionei no evento de click uma função
     event.preventDefault(); //Previne o comportamento padrão do form de recarregar
     var form = document.querySelector("#form-adiciona"); //Truxe o for do html
 //Adicionando os valores coletados do form nas variaveis
     var pessoa = ObtemPessoaDoFormulario(form);
     var pessoaTr = montaTr(pessoa);
-//Adicionado minha Tr na tabela
-     var tabela = document.querySelector("#tabela-pessoa");
-     tabela.appendChild(pessoaTr);
+    var erros = validaPessoa(pessoa);
 
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+
+        return;
+    }
+
+    var tabela = document.querySelector("#tabela-pessoa"); //Adicionado minha Tr na tabela
+    tabela.appendChild(pessoaTr);
+
+    form.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
-//Criando o Objeto Paciente
+//Criando o Objeto pessoa
 function ObtemPessoaDoFormulario(form) {
   var pessoa = {
       nome : form.nome.value,
@@ -21,33 +32,61 @@ function ObtemPessoaDoFormulario(form) {
   }
   return pessoa;
 }
-
 function montaTr(pessoa) {
     var pessoaTr = document.createElement("tr");//Criando uma TR
-    pessoaTr.classList.add("paciente"); //Adicionando uma classe na minha tr
+    pessoaTr.classList.add("pessoa"); //Adicionando uma classe na minha tr
 
-    var nomeTd = document.createElement("td");//Criando uma td
-    nomeTd.classList.add("info-nome");
-
-    var matriculaTd = document.createElement("td");
-    matriculaTd.classList.add("info-matricula");
-
-    var salaTd = document.createElement("td");
-    salaTd.classList.add("info-sala");
-
-    var horarioTd = document.createElement("td");
-    horarioTd.classList.add("info-horario");
-    
-    //Adicionando os valores das labels nas tdSala
-        nomeTd.textContent = pessoa.nome;
-        matriculaTd.textContent = pessoa.matricula;
-        salaTd.textContent = pessoa.sala;
-        horarioTd.textContent = pessoa.horario;
+    var nomeTd = montaTd(pessoa.nome, "info-nome");
+    var matriculaTd = montaTd(pessoa.matricula, "info-matricula");
+    var salaTd = montaTd(pessoa.sala, "info-sala");
+    var horarioTd = montaTd(pessoa.horario, "info-horario");
     //Adicionando as minhas Tds dentro da Tr
-        pessoaTr.appendChild(nomeTd);
-        pessoaTr.appendChild(matriculaTd);
-        pessoaTr.appendChild(salaTd);
-        pessoaTr.appendChild(horarioTd);
+    pessoaTr.appendChild(nomeTd);
+    pessoaTr.appendChild(matriculaTd);
+    pessoaTr.appendChild(salaTd);
+    pessoaTr.appendChild(horarioTd);
 
-    return pessoaTr;
+  return pessoaTr;
+}
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
+function validaPessoa(pessoa) {
+    var erros = [];
+
+    if (!validaNome(pessoa.nome)) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (pessoa.matricula.length == 0) {
+        erros.push("Matricula não pode ser em branco");
+    }
+
+    if(validaMatricula(pessoa.matricula)){
+        erros.push("Matricula Inválida!");
+    }
+
+    if (pessoa.horario.length == 0) {
+        erros.push("O horário não pode ser em branco");
+    }
+
+    if (pessoa.sala.length == 0) {
+        erros.push("A sala não pode ser em branco");
+    }
+
+    return erros;
+}
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
--- js/principal.js
+++ js/principal.js
@@ -1,29 +1,48 @@
 
 //Peguei do HTML A Minha TR com id '#primeira-pessoa'
-var pessoas = document.querySelectorAll("#primeira-pessoa");
+var pessoas = document.querySelectorAll("#pessoa");
 //Peguei do HTML cada td da minha TR com seus respectivas classes
 for (var i = 0; i < pessoas.length; i++) {
-  pessoa = pessoas[i];
-var tdNome = pessoa.querySelector(".info-nome");
-var tdMatricula = pessoa.querySelector(".info-matricula");
-var tdSala = pessoa.querySelector(".info-sala");
-var tdHorario = pessoa.querySelector(".info-horario");
+    pessoa = pessoas[i];
+    var tdNome = pessoa.querySelector(".info-nome");
+    var tdMatricula = pessoa.querySelector(".info-matricula");
+    var tdSala = pessoa.querySelector(".info-sala");
+    var tdHorario = pessoa.querySelector(".info-horario");
 
-var nome = tdNome.textContent;
-var matricula = tdMatricula.textContent;
-var sala = tdSala.textContent;
-var horario = tdHorario.textContent;
-validaPessoa(pessoa);
+    var nome = tdNome.textContent;
+    var matricula = tdMatricula.textContent;
+    var sala = tdSala.textContent;
+    var horario = tdHorario.textContent;
+
+    var MatriculaEhValida = validaMatricula(matricula);
+    var NomeEhValido = validaNome(nome);
+
+    if (!MatriculaEhValida) {
+     console.log("Matricula inválida!");
+     MatriculaEhValida = false;
+     tdMatricula.textContent = "Matricula inválida";
+     pessoa.classList.add("pessoa-invalida");
+ }
+
+ if (!NomeEhValido) {
+     console.log("Nome inválido!");
+     NomeEhValido = false;
+     tdNome.textContent = "Nome inválido";
+     pessoa.classList.add("pessoa-invalida");
+ }
 
 }
-
-function validaPessoa(pessoa) {
-  if(matricula != 8){
-    console.log("Matricula Invalida");
-    pessoa.classList.add("pessoa-invalida");
-  }
-  if(nome.length == 0){
-    console.log("Campo nome Vazio!");
-
-  }
+function validaMatricula(matricula) {
+    if (matricula.length != 8) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validaNome(nome) {
+    if (nome.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
